1a201381758b657ea11f03da4c582cbc26946672
herr-vogel <herr.d.vogel@gmail.com>
2017-04-13 16:03:37 +0200

    feat(Router): add redirect to '/tasks '

--- src/UI/Container.jsx
+++ src/UI/Container.jsx
@@ -4,15 +4,21 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Navbar from './Navbar';
 import TasksContainer from './Tasks/Container';
 
-const Container = () => {
-    return(
-        <Router>
-            <div className='container-fluid'>
-                <Navbar />
-                <Route path='/tasks' component={TasksContainer} />
-            </div>
-        </Router>
-    )
-};
 
-export default Container;
+export default class Container extends React.Component {
+
+    componentDidMount() {
+        this.props.location.pathname === '/' ? this.props.history.push('/tasks') : null;
+    }
+
+    render() {
+        return (
+            <Router>
+                <div className='container-fluid'>
+                    <Navbar />
+                    <Route path='/tasks' component={TasksContainer}/>
+                </div>
+            </Router>
+        )
+    }
+}
