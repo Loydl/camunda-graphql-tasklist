42777f2f805976c1818497fb0a2663589e9c9623
herr-vogel <herr.d.vogel@gmail.com>
2017-09-26 16:24:44 +0200

    refactor(Container): remove old code

--- src/UI/Tasks/Container.jsx
+++ src/UI/Tasks/Container.jsx
@@ -22,18 +22,6 @@ const Container = ({ match }) => {
                     <Route path={`${match.url}/:userId`} component={List} />
                 </main>
             </div>
-
-            {/*<div className='row'>
-                <div className='col-md-2'>
-                    <div className="list-group">
-                        <NavLink activeClassName='active' className='list-group-item' to={`${match.url}/myTasks`}>My Tasks</NavLink>
-                        <NavLink activeClassName='active' className='list-group-item' to={`${match.url}/all`}>All Tasks</NavLink>
-                    </div>
-                </div>
-                <div className='col-md-10'>
-                    <Route path={`${match.url}/:userId`} component={List} />
-                </div>
-            </div>*/}
         </div>
     )
 };
